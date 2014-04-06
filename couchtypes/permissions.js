2cc9e1ee07c4655e63959cdcc2334433cfc14dd4
Yury Proshchenko <spect.man@gmail.com>
2014-04-06 19:12:47 +0800

    Add permissions.isServerOrDatabaseAdmin from CouchDB's /_users/_design/_auth

--- couchtypes/permissions.js
+++ couchtypes/permissions.js
@@ -229,3 +229,33 @@ exports.inherit = function (type) {
         return type.authorize(newValue || {_deleted: true}, oldValue, userCtx, secObj);
     };
 };
+
+
+exports.isServerOrDatabaseAdmin = function() {
+    return function(userCtx, secObj) {
+        // see if the user is a server admin
+        if(userCtx.roles.indexOf('_admin') !== -1) {
+            return true; // a server admin
+        }
+
+        // see if the user a database admin specified by name
+        if(secObj && secObj.admins && secObj.admins.names) {
+            if(secObj.admins.names.indexOf(userCtx.name) !== -1) {
+                return true; // database admin
+            }
+        }
+
+        // see if the user a database admin specified by role
+        if(secObj && secObj.admins && secObj.admins.roles) {
+            var db_roles = secObj.admins.roles;
+            for(var idx = 0; idx < userCtx.roles.length; idx++) {
+                var user_role = userCtx.roles[idx];
+                if(db_roles.indexOf(user_role) !== -1) {
+                    return true; // role matches!
+                }
+            }
+        }
+
+        return false; // default to no admin
+    }
+}
